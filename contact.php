b6219d04be1b7a0300f4247768ef054e4b96fc7a
shanjida101 <azim15-4708@diu.edu.bd>
2025-07-23 08:17:40 +0600

    features

--- contact.php
+++ contact.php
@@ -64,10 +64,10 @@ if(isset($_POST['send'])){
    <h1 class="title">get in touch</h1>
 
    <form action="" method="POST">
-      <input type="text" name="name" class="box" required placeholder="enter your name">
-      <input type="email" name="email" class="box" required placeholder="enter your email">
-      <input type="number" name="number" min="0" class="box" required placeholder="enter your number">
-      <textarea name="msg" class="box" required placeholder="enter your message" cols="30" rows="10"></textarea>
+      <input type="text" name="name" class="box-contact" required placeholder="enter your name">
+      <input type="email" name="email" class="box-contact" required placeholder="enter your email">
+      <input type="number" name="number" min="0" class="box-contact" required placeholder="enter your number">
+      <textarea name="msg" class="box-contact" required placeholder="enter your message" cols="30" rows="10"></textarea>
       <input type="submit" value="send message" class="btn" name="send">
    </form>
 
